de677245f996452566f0eee94d8531f6e0b9bb1d
Wisdom OKechukwu <wisdom.okechukwu1998@gmail.com>
2021-10-08 11:06:04 +0100

    movie update

--- database/seeders/MovieSeeder.php
+++ database/seeders/MovieSeeder.php
@@ -18,61 +18,101 @@ class MovieSeeder extends Seeder
     {
         
         // \App\Models\User::factory(10)->create();
-        Movie::create([
-            'name' => 'THE FOREST',
-            'price' => 320,
-            'picture' => 'assets/images/uploads/mv-it5.jpg',
-            'director' => 'Julian Kelly',
-            'genre' => 'Adventure',
-            'rating' => '8.7',
-            'runtime' =>'2hr 13min',
-        ]);
+        // Movie::create([
+        //     'name' => 'THE FOREST',
+        //     'price' => 320,
+        //     'picture' => 'assets/images/uploads/mv-it5.jpg',
+        //     'director' => 'Julian Kelly',
+        //     'genre' => 'Adventure',
+        //     'rating' => '8.7',
+        //     'runtime' =>'2hr 13min',
+        // ]);
+
+        // Movie::create([
+        //     'name' => 'IT',
+        //     'price' => 500,
+        //     'picture' => 'assets/images/uploads/mv-it7.jpg',
+        //     'director' => 'Jackson Media',
+        //     'genre' => 'SciFi',
+        //     'rating' => '9.1',
+        //     'runtime' =>'1hr 12min',
+        // ]);
+        // Movie::create([
+        //     'name' => 'INCEPTION',
+        //     'price' => 300,
+        //     'picture' => 'assets/images/uploads/mv-it8.jpg',
+        //     'director' => 'Luke Desprito',
+        //     'genre' => 'Action',
+        //     'rating' => '9,0',
+        //     'runtime' =>'3hr 5min',
+        // ]);
+
+        $movie = Movie::find(2);
+        $movie->picture = 'mv-item4.jpg';
+        $movie ->save();
 
         Movie::create([
-            'name' => 'IT',
-            'price' => 500,
-            'picture' => 'assets/images/uploads/mv-it7.jpg',
-            'director' => 'Jackson Media',
-            'genre' => 'SciFi',
-            'rating' => '9.1',
-            'runtime' =>'1hr 12min',
-        ]);
-        Movie::create([
-            'name' => 'INCEPTION',
-            'price' => 300,
-            'picture' => 'assets/images/uploads/mv-it8.jpg',
-            'director' => 'Luke Desprito',
-            'genre' => 'Action',
-            'rating' => '9,0',
-            'runtime' =>'3hr 5min',
-        ]);
-        Movie::create([
-            'name' => 'HARRY POTTER',
-            'price' => 400,
-            'picture' => 'assets/images/uploads/mv-it10.jpg',
-            'director' => 'Julian Kelly',
+            'name' => 'BLUE VELVET',
+            'price' => 95,
+            'picture' => 'assets/images/uploads/mv-item6.jpg',
+            'director' => 'Desmond Young',
             'genre' => 'Adventure',
-            'rating' => '8.3',
-            'runtime' =>'2hr 13min',
+            'rating' => '8.1',
+            'runtime' =>'1hr 13min',
         ]);
         Movie::create([
-            'name' => 'GUARDIANS OF THE GALAXY',
-            'price' => 300,
-            'picture' => 'assets/images/uploads/mv-it11.jpg',
-            'director' => 'Luke Desprito',
-            'genre' => 'Action',
-            'rating' => '7,0',
-            'runtime' =>'3hr 5min',
-        ]);
-
-        Movie::create([
-            'name' => 'THE GOD FATHER',
+            'name' => 'GRAVITY',
             'price' => 700,
-            'picture' => 'assets/images/uploads/mv-it12.jpg',
-            'director' => 'Jackson Lipton',
+            'picture' => 'assets/images/uploads/mv-item7.jpg',
+            'director' => 'The Ruso Brothers',
             'genre' => 'SciFi',
-            'rating' => '10',
-            'runtime' =>'4hr 12min',
+            'rating' => '9.8',
+            'runtime' =>'2hr 45min',
         ]);
+        Movie::create([
+            'name' => 'SOUTHPAW',
+            'price' => 950,
+            'picture' => 'assets/images/uploads/mv-item8.jpg',
+            'director' => 'Helder Postiga',
+            'genre' => 'Action',
+            'rating' => '9.7',
+            'runtime' =>'3hr 30min',
+        ]);
+
+        Movie::create([
+            'name' => 'SPECTRE',
+            'price' => 900,
+            'picture' => 'assets/images/uploads/mv-item10.jpg',
+            'director' => 'Helder Postiga',
+            'genre' => 'Action',
+            'rating' => '9.5',
+            'runtime' =>'3hr 5min',
+        ]);
+
+        Movie::create([
+            'name' => 'THE STRANGER THINGS',
+            'price' => 500,
+            'picture' => 'assets/images/uploads/mv-item11.jpg',
+            'director' => 'The Ruso Brothers',
+            'genre' => 'SciFi',
+            'rating' => '9.3',
+            'runtime' =>'3hr 5min',
+        ]);
+
+        Movie::create([
+            'name' => 'LA LA LAND',
+            'price' => 250,
+            'picture' => 'assets/images/uploads/mv-item12.jpg',
+            'director' => 'Leonard James',
+            'genre' => 'Adventure',
+            'rating' => '8.4',
+            'runtime' =>'2hr 13min',
+        ]);
+
+
+
+
+
+       
     }
 }
