21fc52d5e1f8d5a5fc8f7dc63f8c296ad2b1f94a
Wisdom OKechukwu <wisdom.okechukwu1998@gmail.com>
2021-10-08 10:43:55 +0100

    movie update

--- database/seeders/MovieSeeder.php
+++ database/seeders/MovieSeeder.php
@@ -19,87 +19,60 @@ class MovieSeeder extends Seeder
         
         // \App\Models\User::factory(10)->create();
         Movie::create([
-            'name' => 'INTO THE WILD',
-            'price' => 55,
-            'picture' => 'assets/images/uploads/mv2.jpg',
-            'director' => 'Damian Smith',
+            'name' => 'THE FOREST',
+            'price' => 320,
+            'picture' => 'assets/images/uploads/mv-it5.jpg',
+            'director' => 'Julian Kelly',
             'genre' => 'Adventure',
-            'rating' => '8.5',
-            'runtime' =>'3hr 3min',
+            'rating' => '8.7',
+            'runtime' =>'2hr 13min',
         ]);
 
         Movie::create([
-            'name' => 'THE WALK',
-            'price' => 122,
-            'picture' => 'assets/images/uploads/mv2.jpg',
-            'director' => 'Damian Smith',
-            'genre' => 'Adventure',
-            'rating' => '9.0',
-            'runtime' =>'2hr 3min',
-        ]);
-
-        Movie::create([
-            'name' => 'BLADE RUNNER',
-            'price' => 45,
-            'picture' => 'assets/images/uploads/mv3.jpg',
-            'director' => 'Herold Fuller',
-            'genre' => 'Action',
-            'rating' => '6.8',
-            'runtime' =>'1hr 45min',
-        ]);
-        Movie::create([
-            'name' => 'MULHOLLAND PRIDE',
-            'price' => 200,
-            'picture' => 'assets/images/uploads/mv4.jpg',
-            'director' => 'Ashley Damon',
-            'genre' => 'Adventure',
-            'rating' => '7.0',
-            'runtime' =>'2hr 15min',
-        ]);
-        Movie::create([
-            'name' => 'SKYFALL: EVIL OF BOSS',
-            'price' => 50,
-            'picture' => 'assets/images/uploads/mv5.jpg',
-            'director' => 'Lashley Epinos',
-            'genre' => 'Action',
-            'rating' => '6.9',
-            'runtime' =>'2hr 5min',
-        ]);
-        Movie::create([
-            'name' => 'INTERSTELLAR',
+            'name' => 'IT',
             'price' => 500,
-            'picture' => 'assets/images/uploads/mv-item1.jpg',
-            'director' => 'Norlan Dynamos',
+            'picture' => 'assets/images/uploads/mv-it7.jpg',
+            'director' => 'Jackson Media',
             'genre' => 'SciFi',
-            'rating' => '7.5',
-            'runtime' =>'1hr 35min',
+            'rating' => '9.1',
+            'runtime' =>'1hr 12min',
         ]);
         Movie::create([
-            'name' => 'THE REVENANT',
-            'price' => 205,
-            'picture' => 'assets/images/uploads/mv-item2.jpg',
-            'director' => 'Legend Brothers',
-            'genre' => 'Adventure',
-            'rating' => '8.4',
-            'runtime' =>'3hr 15min',
-        ]);
-        Movie::create([
-            'name' => 'OBLIVION',
-            'price' => 55,
-            'picture' => 'assets/images/uploads/mv-it1.jpg',
-            'director' => 'Willow Smith',
+            'name' => 'INCEPTION',
+            'price' => 300,
+            'picture' => 'assets/images/uploads/mv-it8.jpg',
+            'director' => 'Luke Desprito',
             'genre' => 'Action',
-            'rating' => '8.3',
-            'runtime' =>'1hr 45min',
-        ]);
-        Movie::create([
-            'name' => 'DIE HARD',
-            'price' => 500,
-            'picture' => 'assets/images/uploads/mv-item3.jpg',
-            'director' => 'Kulher Demitri',
-            'genre' => 'SciFi',
             'rating' => '9,0',
             'runtime' =>'3hr 5min',
+        ]);
+        Movie::create([
+            'name' => 'HARRY POTTER',
+            'price' => 400,
+            'picture' => 'assets/images/uploads/mv-it10.jpg',
+            'director' => 'Julian Kelly',
+            'genre' => 'Adventure',
+            'rating' => '8.3',
+            'runtime' =>'2hr 13min',
+        ]);
+        Movie::create([
+            'name' => 'GUARDIANS OF THE GALAXY',
+            'price' => 300,
+            'picture' => 'assets/images/uploads/mv-it11.jpg',
+            'director' => 'Luke Desprito',
+            'genre' => 'Action',
+            'rating' => '7,0',
+            'runtime' =>'3hr 5min',
+        ]);
+
+        Movie::create([
+            'name' => 'THE GOD FATHER',
+            'price' => 700,
+            'picture' => 'assets/images/uploads/mv-it12.jpg',
+            'director' => 'Jackson Lipton',
+            'genre' => 'SciFi',
+            'rating' => '10',
+            'runtime' =>'4hr 12min',
         ]);
     }
 }
